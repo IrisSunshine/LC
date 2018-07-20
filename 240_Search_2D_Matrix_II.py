dbed589d593c31c481da0418392aca8619f57e5f
IrisSunshine <41266229+IrisSunshine@users.noreply.github.com>
2018-07-20 23:41:03 +0800

    240_search a 2d matrix ii (medium)
    https://leetcode.com/problems/search-a-2d-matrix-ii/description/

--- 240_Search_2D_Matrix_II.py
+++ 240_Search_2D_Matrix_II.py
@@ -1,51 +1,6 @@
 
 # coding: utf-8
 
-# In[31]:
-
-class Solution(object):
-    def restoreIpAddresses(self, s):
-        """
-        :type s: str
-        :rtype: List[str]
-        """
-        if len(s) < 4:
-            return []
-        res = []
-        self.restoreIpAddressesHelper(res, s, [], 0)
-        return res
-    
-    def restoreIpAddressesHelper(self, res, s, temp, start):
-        if len(temp) == 4 and start == len(s):
-            ip = '.'.join(temp)
-            res.append(ip)
-            return
-        if len(temp) >= 4 or start >= len(s):
-            return
-        self.restoreIpAddressesHelper(res, s, temp + [s[start]], start+1)
-        if s[start] != '0' and start < len(s) - 1:
-            self.restoreIpAddressesHelper(res, s, temp + [s[start:start+2]], start+2)
-        if s[start] != '0' and start < len(s) - 2 and int(s[start:start+3]) <= 255:
-             self.restoreIpAddressesHelper(res, s, temp + [s[start:start+3]], start+3)
-                        
-if __name__ == '__main__':
-    sol = Solution()
-    s  = "25525511135"
-    print (s, sol.restoreIpAddresses(s))
-
-
-# In[1]:
-
-a=[
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
-  [9,10,11,12]
-]
-print(a)
-
-
-# In[7]:
-
 class Solution(object):
     def searchMatrix(self, matrix, target):
         """
@@ -68,14 +23,3 @@ if __name__ == '__main__':
     matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
     target = 5
     print (sol.searchMatrix(matrix, target))
-
-
-# In[42]:
-
-
-
-
-# In[ ]:
-
-
-
