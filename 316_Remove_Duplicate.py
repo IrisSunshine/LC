6be23cce3df1e7c046084a909672861adc95bb41
IrisSunshine <41266229+IrisSunshine@users.noreply.github.com>
2018-08-01 20:31:39 +0800

    [E14] 316_Remove Duplicate_Letters
    https://leetcode.com/problems/remove-duplicate-letters/

---
+++ 316_Remove_Duplicate.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+class Solution:
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        cs = set(s)
+        dic = {}
+        res = []
+        for c in s:
+            dic[c] = dic.get(c, 0) + 1
+            
+        for c in s:
+            if c in cs:
+                while len(res) and c < res[-1] and dic[res[-1]] > 0:
+                    cs.add(res.pop())
+                res.append(c)
+                cs.remove(c)
+            dic[c] -= 1
+        return ''.join(res)
+
+if __name__ == '__main__':
+    sol = Solution()
+    s = "cbcdcbca"
+    print (sol.removeDuplicateLetters(s))
