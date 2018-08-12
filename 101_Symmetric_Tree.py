062ce9d4903b026eddd96e283ad20db65c5735f3
IrisSunshine <41266229+IrisSunshine@users.noreply.github.com>
2018-08-12 22:49:42 +0800

    [E41] 101_Symmetric Tree (easy)
    https://leetcode.com/problems/symmetric-tree/description/

---
+++ 101_Symmetric_Tree.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        else:
+            return self.isMirror(root.left, root.right)
+
+    def isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        elif left is None or right is None:
+            return False
+
+        elif left.val == right.val:
+            outPair = self.isMirror(left.left, right.right)
+            inPair = self.isMirror(left.right, right.left)
+            return outPair and inPair
+
+        else:
+            return False
